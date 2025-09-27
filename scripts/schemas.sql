39043a68e869f4f2bdc39657a52cc6bf3634c539
Gustavo Cervon <cervon.gus@gmail.com>
2025-09-27 12:38:28 -0600

    Create schemas for architecture medallion

---
+++ scripts/schemas.sql
@@ -0,0 +1,10 @@
+/*
+ * The script set up three schemas in the database:
+ * bronze, silver and gold
+*/
+
+CREATE SCHEMA IF NOT EXISTS bronze;
+
+CREATE SCHEMA IF NOT EXISTS silver;
+
+CREATE SCHEMA IF NOT EXISTS gold;
