7eeca8835fdfa3874604434b177a979d62052964
Gustavo Cervon <cervon.gus@gmail.com>
2026-05-02 21:37:06 -0600

    fix: typo

--- scripts/bronze/ddl.sql
+++ scripts/bronze/ddl.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS bronze.crm_cust_info (
   cst_key VARCHAR(50),
   cst_firstname VARCHAR(50),
   cst_lastname VARCHAR(50),
-  cst_martial_status VARCHAR(50),
+  cst_marital_status VARCHAR(50),
   cst_gndr VARCHAR(50),
   cst_create_date DATE
 );
